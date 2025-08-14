4a244363386e95da9fca08bfbcccf9bdf481f473
Nisha-Bhattarai <c0840054@mylambton.ca>
2025-08-14 16:11:27 -0400

    Add About section to PrimaryHome page

--- frontend/src/pages/PrimaryHome/PrimaryHome.jsx
+++ frontend/src/pages/PrimaryHome/PrimaryHome.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import "./PrimaryHome.css"
+import About from '../../components/About/About'
 
 const PrimaryHome = () => {
   return (
     <div className='profile-container'>
       <Header />
+      <About />
     </div>
   )
 }
