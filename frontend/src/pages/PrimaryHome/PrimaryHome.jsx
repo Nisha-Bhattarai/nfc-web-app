8eb9f9bbd9284293ba269062479eac2d2e113425
Nisha-Bhattarai <c0840054@mylambton.ca>
2025-08-26 16:45:26 -0400

    Add ContactPermissionModal and ExchangeContact Modal to PrimaryHome

--- frontend/src/pages/PrimaryHome/PrimaryHome.jsx
+++ frontend/src/pages/PrimaryHome/PrimaryHome.jsx
@@ -7,16 +7,23 @@ import { FaDownload, FaExchangeAlt } from "react-icons/fa";
 import SocialSection from '../../components/Social/SocialSection'
 import WebsiteSection from '../../components/Website/WebsiteSection'
 import LightboxGallerySection from '../../components/LightboxGallery/LightboxGallerySection'
-import UpcomingEvents from '../../components/Event/UpcomingEvents'
-import OngoingEvents from '../../components/Event/OngoingEvents'
+import ContactPermissionModal from '../../components/ContactPermissionModal/ContactPermissionModal'
+import Modal from '../../components/Modal/Modal'
+import ExchangeContact from '../../components/ExchangeContact/ExchangeContact'
 
 const PrimaryHome = () => {
+  const [open, setOpen] = React.useState(false);
+
   return (
     <div className='profile-container'>
+      <ContactPermissionModal />
       <Header />
       <div className="button-group">
         <Button icon={FaDownload} label="Save Contact" onClick={() => alert("Saved!")} />
-        <Button icon={FaExchangeAlt} label="Exchange Contact" onClick={() => alert("Exchanged!")} />
+        <Button icon={FaExchangeAlt} label="Exchange Contact" onClick={() => setOpen(true)} />
+          <Modal isOpen={open} onClose={() => setOpen(false)}>
+            <ExchangeContact />
+          </Modal>
       </div>
       <About />
       <SocialSection />
